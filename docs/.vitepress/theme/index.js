2b6be7df579c0ed87e3fec18a7bd38753291fa7a
Brett Jankord <bjankord@gmail.com>
2024-10-19 14:01:06 +0000

    Remove old docs directory

--- docs/.vitepress/theme/index.js
+++
@@ -1,8 +0,0 @@
-import DefaultTheme from 'vitepress/theme';
-
-// This import worked when using the simplified vanilla JS version of color-swatch
-import '../../../src/ColorSwatch.js';
-
-// import '../../../dist/color-swatch.es';
-
-export default DefaultTheme;
