2d924f80643b2d7d28c4e8ba800347b2c5be79e3
Brett Jankord <bjankord@gmail.com>
2026-02-08 15:07:49 +0000

    Add base config to vitepress to fix github pages deployment

--- packages/color-swatch-docs/.vitepress/config.js
+++ packages/color-swatch-docs/.vitepress/config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/color-swatch/',
   title: "Color Swatch",
   description: "A web component for displaying color swatches",
   themeConfig: {
